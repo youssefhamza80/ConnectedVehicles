53e9f0c286e8152e34e5806f5f67c266207c51fd
yousifkamal <yousif.kamal@gmail.com>
2021-01-02 18:04:51 +0300

    Added Vehicle Controller Test case

--- ConnectedVehicles-Customer/src/test/java/com/youssef/altenchallenge/ConnectedVehiclesCustomerApplicationTests.java
+++ ConnectedVehicles-Customer/src/test/java/com/youssef/altenchallenge/ConnectedVehiclesCustomerApplicationTests.java
@@ -9,5 +9,4 @@ class ConnectedVehiclesCustomerApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
 }
---
+++ ConnectedVehicles-Vehicle/src/test/java/com/youssef/altenchallenge/ConnectedVehiclesVehicleApplicationTest.java
@@ -0,0 +1,12 @@
+package com.youssef.altenchallenge;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ConnectedVehiclesVehicleApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+}
