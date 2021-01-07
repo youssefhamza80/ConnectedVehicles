150602eec59b47a0f4d12d0c977bddd9145e21ca
yousifkamal <yousif.kamal@gmail.com>
2021-01-07 17:07:03 +0300

    Resolving security Sonar concerns

--- ConnectedVehicles-Vehicle/src/main/java/com/youssef/altenchallenge/controller/VehicleController.java
+++ ConnectedVehicles-Vehicle/src/main/java/com/youssef/altenchallenge/controller/VehicleController.java
@@ -3,7 +3,6 @@ package com.youssef.altenchallenge.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,7 +19,6 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import io.swagger.annotations.ApiOperation;
 
-@CrossOrigin("*")
 @RestController
 @Api(value = "Vehicle Controller APIs", tags = { "Vehicle Controller" })
 public class VehicleController {
